fa25009f19117b95f0d3276a562cbd2e5958dc0b
zoraaaga <fati.sousse12@gmail.com>
2024-01-03 18:40:55 +0100

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,8 +8,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	size_t left_height, right_height;
 
-	return (_height(tree));
+	if (tree == NULL)
+		return 0;
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (left_height > right_height ? left_height : right_height) + 1;
 }
