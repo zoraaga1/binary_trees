90d70135fa47d13a6a822ab3fc68403639f6ba3b
zoraaaga <fati.sousse12@gmail.com>
2024-01-04 18:39:34 +0100

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,10 +11,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	int left_height = (int)binary_tree_height(tree->left);
-	int right_height = (int)binary_tree_height(tree->right);
-
-	return (left_height - right_height);
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
 
 /**
