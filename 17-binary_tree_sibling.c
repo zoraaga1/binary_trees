e2b52fcb3839cb33beed126a65d4d7be316fefb4
zoraaaga <fati.sousse12@gmail.com>
2024-01-04 21:16:03 +0100

    task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,7 +11,7 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
-		return NULL;
+		return (NULL);
 
 	if (node->parent->left == node)
 		return (node->parent->right);
