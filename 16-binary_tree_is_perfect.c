ac832871731324754df5b172c570a3158d08e6b4
zoraaaga <fati.sousse12@gmail.com>
2024-01-04 23:38:24 +0100

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -48,14 +48,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int isperfect_left = 1, isperfect_right = 1;
-
-	if (tree == NULL)
-		return (0);
-	if (tree->left)
-		isperfect_left = binary_tree_is_perfect(tree->left);
-	if (tree->right)
-		isperfect_right = binary_tree_is_perfect(tree->right);
-	if (binary_tree_is_full(tree) && !binary_tree_balance(tree))
-		return (isperfect_left * isperfect_right);
-	return (0);
+	return (tree && binary_tree_is_full(tree) &&
+			binary_tree_height(tree->left) == binary_tree_height(tree->right));
+}
